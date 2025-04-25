34381efc2d8d6f686b5bf49c2da371e3f8d4b9fd
mykola-test1 <mykola.yakovkin@ucu.edu.ua>
2025-04-25 20:18:51 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def get_db_connection():
         connection = mysql.connector.connect(
             host="mysql_db",  # Ensure the MySQL service is correctly configured.
             user="root",
-            password="password",  # Ensure the password is correct.
+            password="password",  # Ensureaksj;dg the password is correct.
             database="test_db"    # Ensure the database exists or is created.
         )
         return connection
